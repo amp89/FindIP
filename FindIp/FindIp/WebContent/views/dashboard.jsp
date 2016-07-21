652668d76d56e03c7b2b3ef33effd1a103d3bd77
Alex Peterson <alexander.peterson89@gmail.com>
2016-07-21 00:21:43 -0600

    getting bad request error on dashboard angular http request

--- FindIp/FindIp/WebContent/views/dashboard.jsp
+++ FindIp/FindIp/WebContent/views/dashboard.jsp
@@ -2,11 +2,36 @@
 
 
 <c:if test="${!empty(user) && user.userType.accessLevel > 0}">
-
-
-IM A DASHBOARD
-
-
+	
+	User Dashboard
+	
+	<div ng-app="showUserDataApp" ng-controller="userController">
+	
+	
+	
+	
+	</div>
+	
+	<script>
+		var app = angular.module("showUserDataApp",[]);
+		app.controller('userController',function($scope,$http){
+			/* $scope.getUserData = {};
+			$scope.getUserData.accessToken = "${accessToken}";
+			 */
+			$scope.userData = {};
+			$http({
+				method:"GET",
+				url:"rest/getLoggedInUserData/"+"${accessToken}",
+				params : {"content-type":"text/plain","Accept" : "application/json"}
+			}).then(function success(response){
+				$scope.userData = response.data;
+				console.log($scope.userData); //TODO remove
+			},function error(response){
+				console.log("User not found.")
+			});
+			
+		});
+	</script>
 </c:if>
 </body>
 </html>
--- FindIp/FindIp/src/controllers/UserRestController.java
+++ FindIp/FindIp/src/controllers/UserRestController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,17 @@ public class UserRestController {
 
 	
 	// TODO this is a test moethod, however it if is usefull i will leave it
-	@RequestMapping(value = "/getLoggedInUserData", method = RequestMethod.GET, produces = "application/json")
-	private CurrentUser getLoggedInUserData(HttpSession session) {
+	@RequestMapping(value = "/getLoggedInUserData/{accessToken}", method = RequestMethod.GET, produces = "application/json")
+	private CurrentUser getLoggedInUserData(HttpSession session, @PathVariable String accessToken) {
 		CurrentUser cu = (CurrentUser) session.getAttribute("currentUserLogin");
-		System.out.println("Rest Controller.  currentUserLogin: " + cu);
+		System.out.println("Rest Controller.  currentUserLogin: " + cu); //TODO remove
+		if(cu != null && cu.getAccessToken().equals(accessToken)){
+			return cu;
+		}else{
+			return null;
+			
+		}
 
-		return cu;
 		// return "{\"TEST\":\"DATAAAAAA\"}";
 
 	}// getLoggedInUserData
